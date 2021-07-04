14202cbfdf8d64566aa18c778c480914bbd06eac
zhangzhen <zhangzhen@smart-haier.com>
2021-07-04 22:09:16 +0800

    local preview

--- app/src/main/java/com/example/webrtcmeetingroom/ChatRoomActivity.java
+++ app/src/main/java/com/example/webrtcmeetingroom/ChatRoomActivity.java
@@ -12,8 +12,11 @@ import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 
 import com.example.webrtcmeetingroom.utils.PermissionUtil;
+import com.example.webrtcmeetingroom.utils.Utils;
 
 import org.webrtc.EglBase;
+import org.webrtc.MediaStream;
+import org.webrtc.RendererCommon;
 import org.webrtc.SurfaceViewRenderer;
 import org.webrtc.VideoTrack;
 
@@ -64,5 +67,52 @@ public class ChatRoomActivity extends AppCompatActivity {
             webRTCManager.joinRoom(this, rootEglBase);
         }
 
+    }
+
+    public void onSetLocalStream(MediaStream stream, String myId) {
+        List<VideoTrack> videoTracks  = stream.videoTracks;
+        if (videoTracks.size()>0){
+            localVideoTrack =videoTracks.get(0);
+        }
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                addView(myId,stream);
+            }
+        });
+
+    }
+
+    private void addView(String myId, MediaStream stream) {
+        SurfaceViewRenderer renderer = new SurfaceViewRenderer(this);
+        renderer.init(rootEglBase.getEglBaseContext(),null);
+        renderer.setScalingType(RendererCommon.ScalingType.SCALE_ASPECT_FIT);
+        renderer.setMirror(true);
+
+        if (stream.videoTracks.size()>0){
+            stream.videoTracks.get(0).addSink(renderer);
+        }
+
+        videoViews.put(myId,renderer);
+        persons.add(myId);
+        wrVideoLayout.addView(renderer);
+        // 宽度和高度
+        int size  = videoViews.size();
+        for (int i =0;i<size;i++){
+            String peerId = persons.get(i);
+            SurfaceViewRenderer surfaceViewRenderer  =videoViews
+                    .get(peerId);
+            if (surfaceViewRenderer!=null){
+                FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT);
+                layoutParams.height  = Utils.getWidth(this,size);
+                layoutParams.height = Utils.getWidth(this,size);
+                layoutParams.leftMargin = Utils.getX(this,size,i);
+                layoutParams.topMargin = Utils.getY(this,size,i);
+                surfaceViewRenderer.setLayoutParams(layoutParams);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/webrtcmeetingroom/connection/PeerConnectionManager.java
+++ app/src/main/java/com/example/webrtcmeetingroom/connection/PeerConnectionManager.java
@@ -5,12 +5,24 @@ import android.content.Context;
 import com.example.webrtcmeetingroom.ChatRoomActivity;
 import com.example.webrtcmeetingroom.socket.JavaWebSocket;
 
+import org.webrtc.AudioSource;
+import org.webrtc.AudioTrack;
+import org.webrtc.Camera1Capturer;
+import org.webrtc.Camera1Enumerator;
+import org.webrtc.Camera2Enumerator;
+import org.webrtc.CameraEnumerator;
 import org.webrtc.DefaultVideoDecoderFactory;
 import org.webrtc.DefaultVideoEncoderFactory;
 import org.webrtc.EglBase;
+import org.webrtc.MediaConstraints;
+import org.webrtc.MediaStream;
 import org.webrtc.PeerConnectionFactory;
+import org.webrtc.SurfaceTextureHelper;
+import org.webrtc.VideoCapturer;
 import org.webrtc.VideoDecoderFactory;
 import org.webrtc.VideoEncoderFactory;
+import org.webrtc.VideoSource;
+import org.webrtc.VideoTrack;
 import org.webrtc.audio.JavaAudioDeviceModule;
 
 import java.util.ArrayList;
@@ -25,8 +37,30 @@ public class PeerConnectionManager {
     private boolean videoEnable;
     private ExecutorService executor;
     private PeerConnectionFactory factory;
-    private Context context;
+    private ChatRoomActivity context;
     private EglBase rootEglBase ;
+    private MediaStream localStream;
+    private AudioSource audioSource;
+    // 音视频轨
+    private VideoTrack localVideoTrack;
+    private AudioTrack localAudioTrack;
+    // 获取摄像头设备
+    private SurfaceTextureHelper surfaceTextureHelper;
+    private VideoCapturer capturerAndroid;
+    // 视频源
+    private VideoSource videoSource;
+
+
+    //    googEchoCancellation   回音消除
+    private static final String AUDIO_ECHO_CANCELLATION_CONSTRAINT = "googEchoCancellation";
+    //    googNoiseSuppression   噪声抑制
+    private static final String AUDIO_NOISE_SUPPRESSION_CONSTRAINT = "googNoiseSuppression";
+
+    //    googAutoGainControl    自动增益控制
+    private static final String AUDIO_AUTO_GAIN_CONTROL_CONSTRAINT = "googAutoGainControl";
+    //    googHighpassFilter     高通滤波器
+    private static final String AUDIO_HIGH_PASS_FILTER_CONSTRAINT = "googHighpassFilter";
+
 
     private PeerConnectionManager(){
         executor = Executors.newSingleThreadExecutor();
@@ -53,9 +87,85 @@ public class PeerConnectionManager {
         executor.execute(new Runnable() {
             @Override
             public void run() {
-                factory = createConnectionFactory();
+                if (factory==null){
+                    factory = createConnectionFactory();
+                }
+                if (localStream==null){
+                    createLocalStream();
+                }
+
             }
         });
+    }
+
+    /**
+     *  本底流的处理，摄像头预览
+     */
+    private void createLocalStream() {
+        localStream = factory.createLocalMediaStream("ARDAMS");
+        // 音频
+        audioSource = factory.createAudioSource(createAudioConstraints());
+
+        localStream.addTrack(localAudioTrack);
+        if (videoEnable){
+            capturerAndroid = createVideoCapture();
+            videoSource = factory.createVideoSource(capturerAndroid.isScreencast());
+            surfaceTextureHelper = SurfaceTextureHelper.create("CaptureThread",rootEglBase.getEglBaseContext());
+            capturerAndroid.initialize(surfaceTextureHelper,context,videoSource.getCapturerObserver());
+            capturerAndroid.startCapture(320,240,10);
+
+            localVideoTrack = factory.createVideoTrack("ARDAMSv0",videoSource);
+            localStream.addTrack(localVideoTrack);
+
+            if (context!=null){
+                context.onSetLocalStream(localStream,myId);
+            }
+
+        }
+
+    }
+
+    private VideoCapturer createVideoCapture() {
+        VideoCapturer videoCapturer = null;
+        if (Camera2Enumerator.isSupported(context)){
+            Camera2Enumerator enumerator =new Camera2Enumerator(context);
+            videoCapturer = createCameraCapture(enumerator);
+        }else {
+            Camera1Enumerator enumerator = new Camera1Enumerator(true);
+            videoCapturer =createCameraCapture(enumerator);
+        }
+        return videoCapturer;
+    }
+
+    private VideoCapturer createCameraCapture(CameraEnumerator enumerator) {
+        //        0   1  front
+        String[] deviceNames =enumerator.getDeviceNames();
+        for (String deviceName:deviceNames){
+            if (enumerator.isFrontFacing(deviceName)){
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName,null);
+                if (videoCapturer!=null){
+                    return videoCapturer;
+                }
+            }
+        }
+
+        for (String deviceName:deviceNames){
+            if (!enumerator.isFrontFacing(deviceName)){
+                VideoCapturer videoCapturer = enumerator.createCapturer(deviceName,null);
+                if (videoCapturer!=null){
+                    return videoCapturer;
+                }
+            }
+        }
+        return null;
+    }
+
+    private MediaConstraints createAudioConstraints() {
+        MediaConstraints audioConstraints = new MediaConstraints();
+        audioConstraints.mandatory.add(new MediaConstraints.KeyValuePair(AUDIO_NOISE_SUPPRESSION_CONSTRAINT,"true"));
+        audioConstraints.mandatory.add(new MediaConstraints.KeyValuePair(AUDIO_AUTO_GAIN_CONTROL_CONSTRAINT,"false"));
+        audioConstraints.mandatory.add(new MediaConstraints.KeyValuePair(AUDIO_HIGH_PASS_FILTER_CONSTRAINT,"true"));
+        return audioConstraints;
     }
 
     /**
